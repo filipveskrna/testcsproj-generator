fd5877504cec6f99cbd345c4deb07a710ab85fcc
filipveskrna <filip.veskrna@hotmail.com>
2022-02-25 21:40:15 +0100

    Feature/initial dotnet tool (#1)
    * Initial gitignore
    
    * Initial vs project
    
    * Fix whitespace in csproj

---
+++ src/TestCsprojGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCsprojGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a dotnet tool.");
+        }
+    }
+}
